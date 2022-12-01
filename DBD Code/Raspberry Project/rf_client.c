304d058cee5d0b8c1a08fa42c50d6050a871a69d
notjon43 <116309463+notjon43@users.noreply.github.com>
2022-12-01 06:34:03 -0500

    Segmentation Fault
    I will ask the SME more about the segmentation fault, I know it is because of how I am referencing addr / dest

--- DBD Code/Raspberry Project/rf_client.c
+++ DBD Code/Raspberry Project/rf_client.c
@@ -1,17 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
 #include <bluetooth/rfcomm.h>
+
+int scan();
+
+int scan()
+{
+    inquiry_info *ii = NULL;
+    int max_rsp, num_rsp;
+    int dev_id, sock, len, flags;
+    int i;
+    char addr[18] = { 0 }; //I need help with understanding how to prevent the segmentation fault
+    char name[248] = { 0 }; //If im not wrong its how im initializing and calling addr
+
+    dev_id = hci_get_route(NULL);
+    sock = hci_open_dev( dev_id );
+    if (dev_id < 0 || sock < 0) {
+        perror("opening socket");
+        exit(1);
+    }   
+
+    len  = 8;
+    max_rsp = 255;
+    flags = IREQ_CACHE_FLUSH;
+         ii = (inquiry_info*)malloc(max_rsp * sizeof(inquiry_info));
+
+    num_rsp = hci_inquiry(dev_id, len, max_rsp, NULL, &ii, flags);
+    if( num_rsp < 0 ) perror("hci_inquiry");
+
+    for (i = 0; i < num_rsp; i++) {
+        ba2str(&(ii+i)->bdaddr, addr);
+        memset(name, 0, sizeof(name));
+        if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
+            name, 0) < 0)
+        strcpy(name, "[unknown]");
+        printf("%s  %s\n", addr, name);
+    }   
+
+    free( ii );
+    close( sock );
+    return &addr;
+}
 
 int main(int argc, char **argv)
 {
     struct sockaddr_rc addr = { 0 };
     int s, status;
-    char dest[18] = "REPALCE AND ENTER MAC ADDRESS HERE"; // this is the address for your bluetooth device!!!!! remember to add the MAC address before compiling
+    char dest;
 
     // allocate a socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    printf("I am starting to scan for devices!\n");
+
+    dest = scan(); //Im certain that this is where the error occurs
+    printf(dest);
 
     // set the connection parameters (who to connect to)
     addr.rc_family = AF_BLUETOOTH;
