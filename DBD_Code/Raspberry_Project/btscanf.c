0af86c9d47c4299c57411f4719e614cce4d71c83
Jhonatan Mendez <mendezjon43@gmail.com>
2023-02-18 13:24:30 -0500

    Added a pair to feature I added some features to btscanf.c so that a user can choose what device they wish to connect to, this can then be used to either the client or server to connect to a device as opposed to adding the MAC address manually I still need to clean up some stuff in the program to make it look nice I wanted to post my progress so far

--- DBD_Code/Raspberry_Project/btscanf.c
+++ DBD_Code/Raspberry_Project/btscanf.c
@@ -7,19 +7,18 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 
-
-
 char (*scanner())[18];
 char addr[8];
 char ar1[4][18];
-
+char ar2[4][1024]; // This will be used for the device names 
+				   // but im tempted to just add all this in a "Jaggaed Array"
 
 char (*scanner())[18]
 {
     inquiry_info *ii = NULL;
     int max_rsp, num_rsp;
     int dev_id, sock, len, flags;
-    int i;
+    int i, inp;
     char name[248];
 
     dev_id = hci_get_route(NULL);
@@ -43,23 +42,44 @@ char (*scanner())[18]
         if (hci_read_remote_name(sock, &(ii+i)->bdaddr, sizeof(name),
             name, 0) < 0)
         strcpy(name, "[unknown]");
+		strcpy(ar2[i], name);
 		strcpy(ar1[i], addr);
 		printf("%s  %s\n", addr, name);
     }
-	printf("Printing the contents of the array !\n\n");
 	for (int i = 0; i < 4; i++){
-		printf("%s\n", ar1[i]);
+		i++;
+		printf("%i\t%s\n",i , ar1[i-1]);
+		i--;
     }
+	printf("Please enter the number of the MAC address you want to pair to:");
+	scanf("%i", &inp);
+	switch (inp) {
+		case 1:
+			printf("You have picked the first device: %s\n", ar1[inp-1]);
+			break;
+		case 2:
+			printf("You have picked the second device: %s\n", ar1[inp-1]);
+			break;
+		case 3:
+			printf("You have picked the third device: %s\n", ar1[inp-1]);
+			break;
+		case 4:
+			printf("You have picked the fourth device: %s\n", ar1[inp-1]);
+			break;
+		default:
+			printf("You didnt pick a valid number try again!\n");
+	}
 	printf("End of loop\n");
-	return ar1;
-    free( ii );
-    close( sock );
+	return &ar1[inp-1];
+    free(ii);
+    close(sock);
 }
 
 int main()
-{
+{	
+	char (*conar1)[18];
 	printf("Calling the function\n");
-	scanner();
-
+	conar1 = scanner();
+	printf("Connecting to: %s\n", *conar1);
 	return 0;
 }
--- DBD_Code/Raspberry_Project/testing.c
+++ DBD_Code/Raspberry_Project/testing.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include "scanner.h"
 
-
+// I need to learn about jag arrays
 int main()
 {
 	char ar1[4][18];
+	int inp;
 
 	for (int i = 0; i < 4;i++){
 		printf("enter a string: ");
@@ -16,9 +16,30 @@ int main()
 	for (int i = 0; i < 4; i++){
 		printf("%s\n", ar1[i]);
 	}
+	printf("Pick which string you want to choose: \n");
+	
+	for (int i = 0; i < 4; i++){
+		i++;
+		printf("%i\t%s\n", i, ar1[i-1]);
+		i--;
+	}
+	printf("Please enter a number from the top!: \n");
+	scanf("%i", &inp);
+
+	switch (inp) {
+		case 1:
+			printf("You have picked the first string: %s\n", ar1[inp-1]);
+			break;
+		case 2:
+			printf("You have picked the second string: %s\n", ar1[inp-1]);
+			break;
+		case 3:
+			printf("You have picked the third string: %s\n", ar1[inp-1]);
+			break;
+		case 4:
+			printf("You have picked the fourth string: %s\n", ar1[inp-1]);
+			break;
+	}
 	printf("End of loop\n");
-	printf("Calling Scanner rn\n");
-	scanner();
 	return 0;
 }
-
