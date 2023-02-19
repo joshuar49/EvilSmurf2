a6714e6de7f4959667df3303a53a5292cce8ff83
Jhonatan Mendez <mendezjon43@gmail.com>
2023-02-19 12:15:50 -0500

    Added our logo to Shell script This is just a cosmetic addition for the demo

--- DBD_Code/Raspberry_Project/pi.sh
+++ DBD_Code/Raspberry_Project/pi.sh
@@ -2,6 +2,35 @@
 
 mkdir DBD2
 cd DBD2
+echo -e "
+                                                            
+                                                            
+DDDDDDDDDDDDD         BBBBBBBBBBBBBBBBB    DDDDDDDDDDDDD        
+D::::::::::::DDD      B::::::::::::::::B   D::::::::::::DDD     
+D:::::::::::::::DD    B::::::BBBBBB:::::B  D:::::::::::::::DD   
+DDD:::::DDDDD:::::D   BB:::::B     B:::::B DDD:::::DDDDD:::::D  
+  D:::::D    D:::::D   B::::B     B:::::B   D:::::D    D:::::D 
+  D:::::D     D:::::D  B::::B     B:::::B   D:::::D     D:::::D
+  D:::::D     D:::::D  B::::BBBBBB:::::B    D:::::D     D:::::D
+  D:::::D     D:::::D  B:::::::::::::BB     D:::::D     D:::::D
+  D:::::D     D:::::D  B::::BBBBBB:::::B    D:::::D     D:::::D
+  D:::::D     D:::::D  B::::B     B:::::B   D:::::D     D:::::D
+  D:::::D     D:::::D  B::::B     B:::::B   D:::::D     D:::::D
+  D:::::D    D:::::D   B::::B     B:::::B   D:::::D    D:::::D 
+DDD:::::DDDDD:::::D  BB:::::BBBBBB::::::B DDD:::::DDDDD:::::D  
+D:::::::::::::::DD   B:::::::::::::::::B  D:::::::::::::::DD   
+D::::::::::::DDD     B::::::::::::::::B   D::::::::::::DDD     
+DDDDDDDDDDDDD        BBBBBBBBBBBBBBBBB    DDDDDDDDDDDDD        
+                                                            
+                                                            
+    ____                        ____   __                ____                   
+   / __ \ ___   ___   ____     / __ ) / /__  __ ___     / __ \ ___  _   __ _____
+  / / / // _ \ / _ \ / __ \   / __  |/ // / / // _ \   / / / // _ \| | / // ___/
+ / /_/ //  __//  __// /_/ /  / /_/ // // /_/ //  __/  / /_/ //  __/| |/ /(__  ) 
+/_____/ \___/ \___// .___/  /_____//_/ \__,_/ \___/  /_____/ \___/ |___//____/  
+                  /_/                                                           
+
+"
 
 echo "Downloading DBD README.txt for convenience"
 wget https://github.com/joshuar49/EvilSmurf2/raw/main/DBD_Code/Raspberry_Project/README.md
