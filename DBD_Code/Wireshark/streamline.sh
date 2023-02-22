a226cd0d0898b230b6a72e2d496a6e64606647c1
joshuar49 <107435186+joshuar49@users.noreply.github.com>
2023-02-21 23:44:55 -0500

    Update streamline.sh

--- DBD_Code/Wireshark/streamline.sh
+++ DBD_Code/Wireshark/streamline.sh
@@ -3,23 +3,15 @@
 filename=client_and_server_message.pcap
 
 while true
-
 do
-
 	if [ -f "$filename" ];
-
 	then
-
 	    echo "$filename has found."
-
 	    strings $filename > output.txt
-
+# Uncomment command below to filter through output.txt for specific strings and send to filter.txt Replace STRING with value of choice
+#	    grep 'hello' output.txt > filter.txt
 	else
-
 	    echo "filename has not been found"
-
 	fi
-
 	sleep 120
-
 done
