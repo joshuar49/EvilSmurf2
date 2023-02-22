b2f1cb4019e4dae063b2986a45d153dc179f4f3b
joshuar49 <107435186+joshuar49@users.noreply.github.com>
2023-02-22 00:26:57 -0500

    Update streamline.sh

--- DBD_Code/Wireshark/streamline.sh
+++ DBD_Code/Wireshark/streamline.sh
@@ -8,7 +8,7 @@ do
 	then
 	    echo "$filename has found."
 	    strings $filename > output.txt
-# Uncomment command below to filter through output.txt for specific strings and send to filter.txt Replace STRING with value of choice
+# Uncomment command below to filter through output.txt for specific strings and send to filter.txt Replace hello with value of choice
 #	    grep 'hello' output.txt > filter.txt
 	else
 	    echo "filename has not been found"
