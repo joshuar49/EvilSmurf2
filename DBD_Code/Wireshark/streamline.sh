97d37c43559172ad7fa35c8f63637756837ebd71
joshuar49 <107435186+joshuar49@users.noreply.github.com>
2023-02-28 23:24:20 -0500

    Update streamline.sh

--- DBD_Code/Wireshark/streamline.sh
+++ DBD_Code/Wireshark/streamline.sh
@@ -2,11 +2,11 @@
 
 while true
 do
-	#searches for all pcap files in directory
+	# Searches for all pcap files in directory
 	for x in *.pcap; do
-	#removes extension from orginal filename used to name output file
+	# Removes extension from orginal filename used to name output file
 	strings $x > "${x:0:${#x}-5}_output.txt"
-	#moves analyzed pcap file out of folder
+	# Moves analyzed pcap file out of folder
 	mv $x /home/dbd/savedpcap 
 	# Uncomment command below to filter through output.txt for specific strings and send to filter.txt Replace hello with string of choice
 	grep 'hello' "${x:0:${#x}-5}_output.txt" > "${x:0:${#x}-5}_filter.txt"
