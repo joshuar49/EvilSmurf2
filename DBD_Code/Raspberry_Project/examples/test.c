5349aa33fa94c64d740fe2edc7940434a6e3dd93
Jhonatan Mendez <mendezjon43@gmail.com>
2023-04-13 01:19:47 -0400

    Merged my private gitea repo to github repo I made some touch up and a lot of moving around from my private repo to here
    In addition i also updated parser.py to now decode PDU messages from
    specific file!

---
+++ DBD_Code/Raspberry_Project/examples/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	printf("Hello this is going to be a test!");
+	return EXIT_SUCCESS;
+}
--- DBD_Code/Raspberry_Project/parser.py
+++ DBD_Code/Raspberry_Project/parser.py
@@ -1,11 +1,22 @@
+from gsmmodem.pdu import decodeSmsPdu
+
 file_name = input("Enter your output file to clean here with the file extension: ")
 with open(file_name, 'r') as file:
     for line in file:
         if line.startswith('Response From Device:'):
             line.strip("\n")
-            text = line.strip('Response From Device:').strip() 
+            index = len("Response From Device: ")
+            text = line.strip('Response From Device:').strip()
             if text:
-                print(f'Response From Device: {text}')
+                if text != "OK" and not text.startswith("+CMGL"):
+                    str(text)
+                    print(decodeSmsPdu(text))
+                else:
+                    print(f'Response From Device: {text}')
         else:
             print(line.strip("\n"))
 
+
+#mess = input("Enter your pdu messaer here and hopefully we will see what happens: ")
+
+#print(decodeSmsPdu("00040B917130161876F00000321013218121610AD9771D242ED3E96539"))
